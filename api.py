55e7d0580d17bcc792a3457c465adf9dde707f0d
Nagendravishnu K <vishnusachin7@hotmail.com>
2023-08-07 20:52:32 +0530

    Adding main

--- api.py
+++ api.py
@@ -1,25 +1,30 @@
 import requests
 from prettytable import PrettyTable
 
-url = "https://randomuser.me/api/"
+def randomUser():
 
-headers = {}
-payload = {}
+    url = "https://randomuser.me/api/"
 
-response = requests.get(url, headers=headers)
+    headers = {}
+    payload = {}
 
-data = response.json() # Assign json response to a variable
+    response = requests.get(url, headers=headers)
 
-table = PrettyTable(["Gender", "City", "Age","version","ID"])
+    data = response.json() # Assign json response to a variable
 
-gender = data["results"][0]["gender"]
-city = data["results"][0]["location"]["city"]
-age = data["results"][0]["dob"]["age"]
-version = data["info"]["version"]
-id = data["results"][0]["id"]["name"]
+    table = PrettyTable(["Gender", "City", "Age","version","ID"])
 
-# Add a row to the table
-table.add_row([gender, city, age,version,id])
+    gender = data["results"][0]["gender"]
+    city = data["results"][0]["location"]["city"]
+    age = data["results"][0]["dob"]["age"]
+    version = data["info"]["version"]
+    id = data["results"][0]["id"]["name"]
 
-# Print the table
-print(table)
+    # Add a row to the table
+    table.add_row([gender, city, age,version,id])
+
+    # Print the table
+   # print(table)
+    return age
+age = randomUser()
+#Age will be printed in main.py
